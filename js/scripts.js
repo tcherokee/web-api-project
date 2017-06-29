6206a64dd2a70e79e8e559fd62d418b1a2306a74
Tolumi Adamson <tolumi.adamson@kenshomedia.com>
2017-06-29 07:39:54 +0100

    New JS Code
    using chained thens to try and circumnavigate the issue I am having….

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,29 @@
 $(document).ready(function() {
   var url = 'http://pokeapi.co/api/v2/pokemon/';
+
   var pokemonOptions = {
     limit:2
   }
-  var pokemonName;
-  var pokemonURLs = [];
-  var pokemons = [];
 
-  function getPokemon(data) {
-    var pokemonResults = data.results;
-
-    $.each(pokemonResults, function(i, index){
-      // console.log(index);
-      pokemonURLs.push(index.url);
-    });
-
-    $.when(...pokemonURLs).done(function(){
-      getPokemonDetails(arguments);
+  $.when($.getJSON(url, pokemonOptions))
+    .then(function(data){
+      return data.results;
     })
-  }
+    .then(function(data){
+      return data.map(data => data.url);
+    })
+    .then(function(data){
+      return $.when(...data.map(url => $.getJSON(url).then(data => [data.name, data.stats, data.abilities])));
 
-  function getPokemonDetails(url){
-    var promises = [];
-    $.each(url, function(i, index){
+      // return $.when(...data.map(function(url){
+      //   $.getJSON(url).then(function(poke){
+      //       poke.name;
+      //     })//End Then
+      //   })//End Map
+      // )// End When
+    })
+    .then(function(data){
+      // data.map()
+    })
 
-      $.getJSON(index, function(data){
-        var pokemon = [data.name, data.stats];
-        var abilityURLs = [];
-        $.each(data.abilities, function(a, abilities){
-          abilityURLs.push(abilities.ability.url)
-        });
-
-        pokemon.push(abilityURLs);
-
-        $.when(...pokemon).done(function(){
-          pokemons.push(arguments);
-        });
-      }).done(function(){promises.push(pokemons)});
-
-    });
-
-    $.when(...promises).done(function(){
-      console.log(arguments);
-    });
-  }
-
-  $.getJSON(url, pokemonOptions, getPokemon);
 })
