2663c609878da049262bb8186a01e79d078684a7
Tolumi Adamson <tolumi@gmail.com>
2017-06-30 11:44:53 +0100

    Updated Working Code
    Used Promises and Resolves to fix the issue.

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,39 @@ $(document).ready(function() {
     limit:2
   }
 
-  $.when($.getJSON(url, pokemonOptions))
-    .then(function(data){
-      return data.results;
-    })
-    .then(function(data){
-      return data.map(data => data.url);
-    })
-    .then(function(data){
-      return $.when(...data.map(url => $.getJSON(url).then(data => [data.name, data.stats, data.abilities])));
+  function getPokemonData(data){
+    return Promise.all(data
+              .map(url => $.getJSON(url)
+                .then(function(data){
+                  var p = Promise.resolve(getPokemonAbility(data.abilities));
 
-      // return $.when(...data.map(function(url){
-      //   $.getJSON(url).then(function(poke){
-      //       poke.name;
-      //     })//End Then
-      //   })//End Map
-      // )// End When
-    })
+                  return p.then(pokemonAbilities => [
+                    data.sprites.front_shiny,
+                    data.name,
+                    data.stats
+                      .map(statistic => statistic.stat.name + ':' + statistic.base_stat),
+                    pokemonAbilities
+                  ])
+                })
+              ))
+  }
+
+  function getPokemonAbility(ability){
+    return Promise.all(ability
+              .map(pokemonAbility => $.getJSON(pokemonAbility.ability.url)
+                .then(abilityData => abilityData.name)))
+  }
+
+  $.when($.getJSON(url, pokemonOptions))
+    .then(data => data.results)
+    .then(data => data
+      .map(data => data.url))
+    .then(getPokemonData)
     .then(function(data){
-      // data.map()
+      var p = Promise.resolve(data);
+      p.then(function(v) {
+        console.log(v[1]); // 1
+      });
     })
 
 })
